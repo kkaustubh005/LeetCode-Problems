f9aebe8248de8c6941ede235d5f6c70f37071b8c
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-06-05 13:00:21 -0400

    Update move-zeroes.java

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -1,24 +1,28 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int i=0;
-        int count=0;
-        for(int x=0;x<nums.length;x++){
-            if(nums[x]==0){
-                ++count;
-            }
-        }
-        int check=0;
-        while(i<nums.length-1){
-            if(nums[i]==0 && check<=count){
-                ++check;
+        int k = nums.length-1;
+        while(i<k){
+            if(nums[i]==0){
                 for(int j=i+1;j<nums.length;j++){
                     nums[j-1]=nums[j];
                 }
-                nums[nums.length-1]=0;
+                nums[k]=0;
+                --k;
             }else{
                 ++i;
             }
         }
+        
+        
+       /* More optimised code 
+       int index=0;
+    		for (int i=0;i<nums.length;i++){
+    			if (nums[i]!=0) nums[index++]=nums[i];
+    		}
+    		while(index<nums.length){
+    			nums[index++]=0;
+    		}*/
         
     }
 }
