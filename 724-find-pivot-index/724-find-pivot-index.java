8d69d99085a5b0df1b0c16ab66c477993d914694
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-07-09 19:45:51 -0400

    Time: 391 ms (9.17%), Space: 43.7 MB (84.23%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,18 +1,41 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int sumLeft=0,sumRight=0;
+        // int sumLeft=0,sumRight=0;
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=0;j<i;j++){
+        //         sumLeft=sumLeft+nums[j];
+        //     }
+        //     for(int k=i+1;k<nums.length;k++){
+        //         sumRight=sumRight+nums[k];
+        //     }
+        //     if(sumLeft==sumRight){
+        //         return i;
+        //     }
+        //     sumLeft=0;
+        //     sumRight=0;
+        // }
+        // return -1;
+        int sum=0;
         for(int i=0;i<nums.length;i++){
-            for(int j=0;j<i;j++){
-                sumLeft=sumLeft+nums[j];
+            sum=sum+nums[i];
+        }
+        int sumLeft=0;
+        int sumRight=0;
+        if(sum-nums[0]==0){
+            return 0;
+        }
+        for(int j=1;j<nums.length;j++){
+            sumLeft=sumLeft+nums[j-1];
+            sumRight=sum-sumLeft-nums[j];
+            
+            System.out.println(j+"-"+nums[j]);
+            System.out.println("sum "+sumRight);
+            System.out.println("sumLeft "+sumLeft);
+            if(sumRight==sumLeft){
+                return j;
             }
-            for(int k=i+1;k<nums.length;k++){
-                sumRight=sumRight+nums[k];
-            }
-            if(sumLeft==sumRight){
-                return i;
-            }
-            sumLeft=0;
-            sumRight=0;
+            sumRight=sum;
+            
         }
         return -1;
     }
