758297267267ac9705d64f6fd7c8702046e77131
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-07-11 21:21:51 -0400

    Time: 33 ms (12.27%), Space: 49.9 MB (7.40%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    private String transform(String s){
+        Map<Character, Integer> map = new HashMap<>();
+        StringBuilder builder = new StringBuilder();
+        
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(!map.containsKey(c)){
+                map.put(c,i);
+            }
+            builder.append(Integer.toString(map.get(c)));
+            builder.append(" ");                       
+        }
+        return builder.toString();
+        
+        
+    }
+    public boolean isIsomorphic(String s, String t) {
+        return transform(s).equals(transform(t));
+    }
+}
