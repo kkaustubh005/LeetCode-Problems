cda1d93d0355f5c9ce912b616542a2e9abc2476e
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-07-16 00:06:20 -0400

    Time: 0 ms (100.00%), Space: 42.3 MB (5.70%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -17,18 +17,18 @@ class Solution {
             current=current.next;
             ++count;
         }
-        System.out.println(count);
+        //System.out.println(count);
         if(count%2==0){
             result=(count/2)+1;
         }else{
             count=count-1;
             result=(count/2)+1;
         }
-        System.out.println(result);
+        //System.out.println(result);
        current=head;
         int count1=1;
         while(count1<result){
-            System.out.println(current.val);
+            //System.out.println(current.val);
             current=current.next;
             ++count1;
         }
