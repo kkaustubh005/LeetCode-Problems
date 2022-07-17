92b0cca6e49489e32cc497bfa5ab27467e6f4936
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-07-17 12:02:22 -0400

    Time: 6 ms (53.40%), Space: 42.3 MB (51.80%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        if(s==null || s.length()==0) return 0;
+        HashSet<Character> set = new HashSet<>();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(set.contains(s.charAt(i))){
+                set.remove(s.charAt(i));
+                count++;
+            }else{
+                set.add(s.charAt(i));
+            }
+        }
+        if(set.isEmpty()){
+            return count*2;
+        }else{
+            return count*2+1;
+        }
+    }
+}
