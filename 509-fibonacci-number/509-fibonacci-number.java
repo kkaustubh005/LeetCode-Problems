7c06800e7bad03fffb1ad2d628f991e456b04c49
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-07-20 00:10:24 -0400

    Time: 1 ms (42.02%), Space: 40.7 MB (63.80%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    private Map<Integer, Integer> map = new HashMap<>(Map.of(0,0,1,1));
+    
+    public int fib(int n) {
+        if(map.containsKey(n)){  //Memoization
+            return map.get(n);
+        }
+        map.put(n, fib(n-1) + fib(n-2));
+        return map.get(n);
+       
+    }
+}
