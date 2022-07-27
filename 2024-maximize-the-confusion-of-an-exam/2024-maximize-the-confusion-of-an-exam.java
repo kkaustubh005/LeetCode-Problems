1b86d21dbaf95486aee3940469fa350fdeb88a3c
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-07-27 15:14:52 -0400

    Time: 10 ms (96.41%), Space: 42.6 MB (93.95%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,16 +1,17 @@
-class Solution {
+class Solution { //Sliding window
     public int maxConsecutiveAnswers(String answerKey, int k) {
         char[] key = answerKey.toCharArray();
         int len = answerKey.length();
         int start=0, end=0, tCount=0, fCount=0, maxCount=0, maxLength=0;
-        
+        //maxCount will store the count of maximum T or F characters are present consecutively.
         for(end=0;end<len;end++){
             if(key[end]=='T'){
                 maxCount=Math.max(maxCount, ++tCount);
             }else{
                 maxCount=Math.max(maxCount, ++fCount);
             }
-            while(end-start-maxCount+1>k){
+            //Below while condition, used to check how many values are there other than max consecutive characters. If they're more than k, then remove them from the sliding window by incrementing start pointer. Max k changes are allowed.
+            while(end-start-maxCount+1>k){  
                 if(key[start]=='T'){
                     tCount--;
                 }else{
