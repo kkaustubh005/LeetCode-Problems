105d9ec3e5a070f93fe3dd12463665d09259a40d
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-08-13 12:45:16 -0400

    Time: 2 ms (81.54%), Space: 41.5 MB (52.52%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -3,15 +3,10 @@ class Solution {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
         for(int n: stones){
             maxHeap.add(n);
-            //System.out.println(maxHeap.peek());
         }       
-        //System.out.println(maxHeap.size());
         while(maxHeap.size()>1){
-            //System.out.println("first "+maxHeap.peek());
             int head = maxHeap.poll();
-            //System.out.println("second "+maxHeap.peek());
             int a = maxHeap.poll();
-            //System.out.println(maxHeap.size());
             if(head!=a){
                 int node = head-a;
                 maxHeap.add(node);
