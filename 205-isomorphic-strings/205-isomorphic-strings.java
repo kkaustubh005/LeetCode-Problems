d50bb92fe56d66211f1be76f2b37d386f4e68d1f
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-08-19 18:37:25 -0400

    Time: 20 ms (29.02%), Space: 42.9 MB (61.85%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -1,8 +1,7 @@
 class Solution {
     private String transform(String s){
         Map<Character, Integer> map = new HashMap<>();
-        StringBuilder builder = new StringBuilder();
-        
+        StringBuilder builder = new StringBuilder();       
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
             if(!map.containsKey(c)){
@@ -11,10 +10,9 @@ class Solution {
             builder.append(Integer.toString(map.get(c)));
             builder.append(" ");                       
         }
-        return builder.toString();
-        
-        
+        return builder.toString();        
     }
+    
     public boolean isIsomorphic(String s, String t) {
         return transform(s).equals(transform(t));
     }
