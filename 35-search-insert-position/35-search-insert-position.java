8cdc6e9a86151090a3adcbd55345f0765239a357
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-09-17 17:50:10 -0400

    Time: 12 ms (15.10%), Space: 43.8 MB (21.00%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        // int check=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==target){
+        //         check=1;
+        //     }
+        // }
+        int mid, left=0,right=nums.length-1;
+        while(left<right){
+            mid = left+(right-left)/2;
+            System.out.println(mid);
+            if(nums[mid]==target) return mid;
+            if(target<nums[mid]) right=mid;
+            else left=mid+1;
+        }
+        System.out.println("Left:"+left);
+        System.out.println("Right:"+right);
+        if(nums[left]<target) return left+1;
+        else return left;
+    }
+}
