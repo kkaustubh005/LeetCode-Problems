2b721a19900cae8d0cb9151e44c1c642e5064184
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-09-16 21:00:39 -0400

    Time: 2 ms (65.07%), Space: 54.3 MB (44.78%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] findBall(int[][] grid) {
-
         int[] result = new int[grid[0].length]; 
         for(int col=0;col<grid[0].length;col++){          
             result[col]=dfs(grid,0,col);
@@ -12,39 +11,16 @@ class Solution {
         int rowLength = grid.length;
         int colLength = grid[0].length;
         
-        //boundary conditions
         if(row==rowLength){
             return col;
-        }
-        
-        //start and end condition(edge case)
-        // if(grid[row][colLength-1]==1 || grid[row][0]==-1){
-        //     //System.out.println("start ball: "+start);
-        //     //result.add(start,-1);
-        //     return -1;
-        // }
-            
-        /*if(grid[rowLength-1][col]==1 && grid[rowLength-1][col+1]==1){
-            System.out.println("add start ball: "+start);
-            System.out.println("ans: "+col);
-            result.add(start,col);
-            return;
-        }
-        
-        if(grid[rowLength-1][col] != 1 && grid[rowLength-1][col-1] !=1){
-            System.out.println("add start ball: "+start);
-            //System.out.println("ans: "+col-1);
-            result.add(start,col);
-            return;
-        }*/
-        
-        if(grid[row][col]==1 && col+1<grid[0].length && grid[row][col+1]==1){
+        }        
+        if(grid[row][col]==1 && col+1<colLength && grid[row][col+1]==1){
             return dfs(grid,row+1,col+1);
         }
-        else if(grid[row][col]==-1 && col-1>=0 && grid[row][col-1]==-1){
+        if(grid[row][col]==-1 && col-1>=0 && grid[row][col-1]==-1){
             return dfs(grid,row+1,col-1);
-        }
+        }    
         
-      return -1;  
+        return -1;  
     }
 }
