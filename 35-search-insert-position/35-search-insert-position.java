4c64a0d5b91d6794f00aef6e5926bc5dba2db371
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-09-17 17:52:20 -0400

    Time: 3 ms (15.10%), Space: 43.4 MB (59.72%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,11 +1,5 @@
-class Solution {
+class Solution {  //Binary search algo
     public int searchInsert(int[] nums, int target) {
-        // int check=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]==target){
-        //         check=1;
-        //     }
-        // }
         int mid, left=0,right=nums.length-1;
         while(left<right){
             mid = left+(right-left)/2;
@@ -14,9 +8,9 @@ class Solution {
             if(target<nums[mid]) right=mid;
             else left=mid+1;
         }
-        System.out.println("Left:"+left);
-        System.out.println("Right:"+right);
-        if(nums[left]<target) return right+1;
-        else return right;
+        //condition for the absence of element
+        //condition for the last element
+        if(nums[left]<target) return left+1;
+        else return left; 
     }
 }
