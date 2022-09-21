e17510dc730b45047e44b5828fa1a014ca85ab13
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-09-21 12:39:57 -0400

    Time: 122 ms (17.42%), Space: 69.8 MB (25.92%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -4,6 +4,7 @@ class Solution {
         String[] sArray= s.split(" ");
         for(int i=0;i<sArray.length;i++){
             String nstr="";
+            //Reverse string logic
             for(int j=0;j<sArray[i].length();j++){
                 char ch = sArray[i].charAt(j);
                 nstr=ch+nstr;
@@ -11,6 +12,7 @@ class Solution {
             builder.append(nstr);
             builder.append(" ");
         }
+        //Removing the last whitespace from the string builder object
         builder.setLength(builder.length() - 1);
         return builder.toString();
     }
