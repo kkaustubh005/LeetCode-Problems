fdb5e4d419a43b5e8f2c12e1729c150e752471ab
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-09-21 01:08:55 -0400

    Time: 3 ms (47.46%), Space: 54.3 MB (79.50%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,6 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0;
+        /*int i=0;
         int k = nums.length-1;
         while(i<k){
             if(nums[i]==0){
@@ -12,16 +12,16 @@ class Solution {
             }else{
                 ++i;
             }
-        }
+        }*/
         
         
-       /* More optimised code 
+       // More optimised code 
        int index=0;
     		for (int i=0;i<nums.length;i++){
     			if (nums[i]!=0) nums[index++]=nums[i];
     		}
     		while(index<nums.length){
     			nums[index++]=0;
-    		}*/
+    		}
     }
 }
