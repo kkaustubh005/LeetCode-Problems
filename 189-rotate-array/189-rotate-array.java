55a92b66572e740bc3d9b32455bb7574f1ea69c1
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2022-09-21 00:59:01 -0400

    Time: 1 ms (87.95%), Space: 65.4 MB (5.07%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,5 +1,6 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
+    public void rotate(int[] nums, int k) {      
+        //Brute force
         /*for(int i=0;i<k;i++){
             int last=nums[nums.length-1];
             int length=nums.length;
@@ -11,13 +12,29 @@ class Solution {
             }
             nums[0]=last;
         }     */
-        //Using extra array
+        
+      /*  //Using extra array
         int[] a = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             a[(i + k) % nums.length] = nums[i];
         }
         for (int i = 0; i < nums.length; i++) {
             nums[i] = a[i];
+        }*/
+        
+        //Reverse
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);                
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
