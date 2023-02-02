7eb94fef88bfac7eab497d9ac47956ed920b6a21
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2023-02-01 19:44:16 -0500

    Time: 21 ms (45.80%), Space: 44.2 MB (20.16%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       /* s=s.toLowerCase();
+        s=s.replaceAll("[^a-z0-9]", "");
+        //System.out.println(s);
+        char[] array = s.toCharArray();
+        int i=0;
+        int j=array.length-1;
+        while(i<j){
+            if(array[i]==array[j]){
+                ++i;
+                --j;
+            }else{
+                return false;
+            }
+        }
+        return true;*/
+        
+        s=s.toLowerCase();
+        s=s.replaceAll("[^a-z0-9]","");
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i=0;i<s.length();i++){
+            stack.push(s.charAt(i));
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            sb.append(stack.pop());
+        }
+        if(s.equals(sb.toString()))
+            return true;
+        else
+            return false;
+        
+    }
+}
