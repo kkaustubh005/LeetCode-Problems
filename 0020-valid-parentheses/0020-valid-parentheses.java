56e013af0f3af64b78fae9636bdecdf0bf9fc913
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2023-02-06 20:00:58 -0500

    Time: 1 ms (99.22%), Space: 40 MB (91.93%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] cArray = s.toCharArray();
+        //Deque<Character> stack = new ArrayDeque<Character>();
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i=0;i<cArray.length;i++){
+            if(cArray[i] == '(')
+                stack.push(cArray[i]);
+            else if(cArray[i] == ')'){
+                if(!stack.empty() && stack.peek() == '(') stack.pop();
+                else return false;
+            }
+            else if(cArray[i] == '{')
+                stack.push(cArray[i]);
+            else if(cArray[i] == '}'){
+                if(!stack.empty() && stack.peek() == '{') stack.pop();
+                else return false;
+            }  
+            else if(cArray[i] == '[')
+                stack.push(cArray[i]);
+            else if(cArray[i] == ']'){
+                if(!stack.empty() && stack.peek() == '[') stack.pop();
+                else return false;
+            }     
+        }
+        return stack.empty();
+    }
+}
