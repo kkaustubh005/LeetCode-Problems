83cb39ab8b5233331d22b3403f48825a5136cf29
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2023-03-12 19:01:26 -0400

    Time: 0 ms, Memory: 39.9 MB - LeetHub

---
+++ climbing-stairs/climbing-stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    private Map<Integer, Integer> count = new HashMap<>(Map.of(1,1,2,2,3,3));
+    public int climbStairs(int n) {
+        if(count.containsKey(n)){
+                return count.get(n);
+        }
+        count.put(n, climbStairs(n-1)+climbStairs(n-2));
+        return count.get(n);
+    }
+}
