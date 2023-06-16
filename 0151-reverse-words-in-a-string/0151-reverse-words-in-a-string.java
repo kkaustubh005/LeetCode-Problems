f4cd265f96a75cb0c6a068ff91fe13142299b8f4
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2023-06-16 14:16:17 -0400

    Time: 7 ms (71.38%), Space: 43.3 MB (49.51%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,29 +1,14 @@
 class Solution {
     public String reverseWords(String s) {
         String[] arr = s.split(" ");
-        int i=0;
-        
         String temp="";
-        //ArrayList<String> arr1 = Arrays.asList(arr);
         List<String> arr1 = new ArrayList<String>(Arrays.asList(arr));
         
         arr1.removeAll(Arrays.asList(""));
-        System.out.println(arr1);
-        // for(int x=0;x<arr1.size();x++){
-        //     if(arr1.get(" ")){
-        //         //arr1.remove(arr[x]);
-        //         System.out.println("Yo");
-        //     }
-        // }
-        
-        // for(String b:arr1){
-        //     if(b==" ")
-        //         arr1.remove(b);
-        // }
-        //System.out.println(arr1);
-        
         String[] str = arr1.toArray(new String[arr1.size()]);
+        int i=0;
         int j=str.length-1;
+        
         while(i<j){
             temp=str[i];
             str[i]=str[j];
@@ -32,19 +17,12 @@ class Solution {
             --j;
         }
         
-        
-        //System.out.println(arr.length);
-        
         StringBuilder sb = new StringBuilder();
         for(int a=0; a<str.length;a++){
-            // if(arr[a]==" ")
-            //     continue;
             sb.append(str[a]);
             sb.append(" ");
         }
-        System.out.println(sb.toString());
         sb.deleteCharAt(sb.length()-1);
-        //sb.deleteCharAt(sb.length()-1);
         return sb.toString();
     }
 }
