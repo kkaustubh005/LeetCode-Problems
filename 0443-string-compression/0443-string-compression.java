27ade32c2e229a434ddbddf8e00dc61ddcc99a21
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2023-06-25 11:55:22 -0400

    Time: 1 ms (96.80%), Space: 43.2 MB (48.65%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,6 +1,6 @@
 class Solution {
     public int compress(char[] chars) {
-        
+  /*      
         int count=0;
         StringBuilder sb = new StringBuilder();
         int i=0;
@@ -26,5 +26,29 @@ class Solution {
         }      
         
         return cArr.length;
+*/
+        //Without using extra space
+        
+        int count=0;
+        int i=0;
+        int counter=0;
+        
+        while(i<chars.length){
+            int j=i;
+            while(j<chars.length && chars[j]==chars[i]){
+                ++count;
+                ++j;
+            }
+            chars[counter++]=chars[i];
+            if(count!=1){
+                char[] x = Integer.toString(count).toCharArray();
+                for(char a:x)
+                   chars[counter++]=a;          
+            }
+            count=0;
+            i=j;
+        }
+        return counter;
+        
     }
 }
