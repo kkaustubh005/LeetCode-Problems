653faaaab7ffb0efc26ff73efbbb6cd2d2f4d221
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2023-06-30 15:28:01 -0400

    Time: 2 ms (100.00%), Space: 49.7 MB (99.09%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -16,21 +16,18 @@
 class Solution {
     private int count=0;
     private int max;
-    //private int initial= root.val;
-    private void preorder(TreeNode root,int max,int initial){
+    private void preorder(TreeNode root,int max){
         if(root==null) return;
         if(root.val>=max){
             ++count;
             max=root.val;
         }
-        // if(root.left==null && root.right==null)
-        //     max=initial;
-        preorder(root.left,max,root.val);
-        preorder(root.right,max,root.val);
+        preorder(root.left,max);
+        preorder(root.right,max);
         
     }
     public int goodNodes(TreeNode root) {
-        preorder(root,root.val,root.val);
+        preorder(root,root.val);
         return count;
     }
 }
