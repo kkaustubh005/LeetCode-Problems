df26371cc97d72b069783707824718794b77e832
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2023-08-21 15:32:56 -0400

    Time: 13 ms (77.60%), Space: 48 MB (15.46%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,18 +4,16 @@ class Solution {
         for(int a: nums)
             map.put(a, map.getOrDefault(a,0)+1);
         
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(
-        (n1, n2) -> map.get(n1) - map.get(n2));
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(
+        (n1, n2) -> map.get(n2) - map.get(n1));
         
         for(int n: map.keySet()) {
-            minHeap.add(n);
-            if(minHeap.size()>k)
-                minHeap.poll();
+            maxHeap.add(n);
         }
         
         int[] top = new int[k];
         for(int i=k-1; i>=0; i--)
-            top[i]=minHeap.poll();
+            top[i]=maxHeap.poll();
         
         return top;
     }
