028b654643a2b6eb566d6daac902f949e8ec0220
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2023-10-07 23:24:31 -0400

    Time: 32 ms (38.30%), Space: 53.2 MB (33.19%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,31 +1,55 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        HashMap<int [], Integer> map = new HashMap<>();
+       /* HashMap<int [], Integer> map = new HashMap<>();
         
         for(int[] point: points){
-            //int distance = (int)Math.sqrt((point[0]*point[0]) + (point[1]*point[1]));
             int distance = (point[0]*point[0]) + (point[1]*point[1]);
             map.put(point, distance);
         }
         
-        PriorityQueue<int[]> minHeap = new PriorityQueue<int[]>((n1,n2) -> {
-            if(map.get(n1)>map.get(n2))
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<int[]>((n1,n2) -> {
+            if(map.get(n1)>map.get(n2))     //If we want to swap then return 1 else -1
                 return -1;
             else
                 return 1;
         });
         
+        //PriorityQueue<int[]> maxHeap = new PriorityQueue<int[]>((n1,n2) -> map.get(n2) - map.get(n1));
+        
         for(int [] point: points){
-            minHeap.add(point);
-            if(minHeap.size()>k)
-                minHeap.poll();
+            maxHeap.add(point);
+            if(maxHeap.size()>k)
+                maxHeap.poll();
         }
         
         int[][] result = new int[k][];
         int count=0;
-        while(minHeap.size()>0){
-            result[count++]=minHeap.poll();
+        while(maxHeap.size()>0){
+            result[count++]=maxHeap.poll();
+        }
+        return result;*/
+        
+        //Better Solution
+        
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> {
+            if((a[0]*a[0]+a[1]*a[1]) > (b[0]*b[0]+b[1]*b[1]))
+                return -1;  //dont swap (descending order)
+            else
+                return 1;
+        });
+     
+        for(int [] point: points){
+            maxHeap.add(point);
+            if(maxHeap.size()>k)
+                maxHeap.poll();
+        }
+        
+        int[][] result = new int[k][];
+        int count=0;
+        while(maxHeap.size()>0){
+            result[count++]=maxHeap.poll();
         }
         return result;
+        
     }
 }
