6108d6517b8cdce1b2ff5343cb2d52af4001c0da
Kaustubh Karanjkar <kkaustubh005@gmail.com>
2023-11-07 20:25:55 -0500

    Time: 7 ms (36.85%), Space: 46.4 MB (92.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,60 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
-        /* Time Limit Exceeded --------
-        
-        List<List<String>> fList = new ArrayList<>();
-        for(int i=0;i<strs.length;i++){
-            //System.out.println(i);
-            if(strs[i]!=null){
-            HashMap<Character,Integer> map = new HashMap<>();
-            char[] cArray = strs[i].toCharArray();
-            for(char x:cArray){
-                if(map.containsKey(x)){
-                    map.put(x,map.get(x)+1);
-                }else{
-                    map.put(x,1);
-                }
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String str: strs){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            if(map.containsKey(String.valueOf(arr)))
+                map.get(String.valueOf(arr)).add(str);
+            else{
+                map.put(String.valueOf(arr), new ArrayList<String>());
+                map.get(String.valueOf(arr)).add(str);
             }
-                List<String> tList = new ArrayList<>();
-                tList.add(strs[i]);
-                for(int j=i+1;j<strs.length;j++){
-                    if(strs[j]!=null){
-                    HashMap<Character,Integer> tmap = new HashMap<>(); 
-                    char[] tArray = strs[j].toCharArray();
-                    for(char y:tArray){
-                        if(tmap.containsKey(y)){
-                            tmap.put(y,tmap.get(y)+1);
-                        }else{
-                            tmap.put(y,1);
-                            }
-                    }
-                    if(map.equals(tmap)){
-                        tList.add(strs[j]);
-                        strs[j]=null;
-                    }
-                    }
-                }
-                fList.add(tList);
-                strs[i]=null;
-                
-                
-        }
-    }
-        return fList;*/
-        
-        if(strs.length==0) return new ArrayList();
-        HashMap<String,List> map = new HashMap<>();
-        for(String s:strs){
-            char[] ch = s.toCharArray();
-            Arrays.sort(ch);
-            String key = String.valueOf(ch);
-            if(!map.containsKey(key)){
-                map.put(key,new ArrayList());
-            }
-            map.get(key).add(s);
         }
         return new ArrayList(map.values());
-        
     }
 }
